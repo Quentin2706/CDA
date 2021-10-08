4a8923d0aa916b8c7190e1c15549294b93ad48ef
Quentin2706 <quentin.balair2706@gmail.com>
2021-10-08 16:35:32 +0200

    generateur script

---
+++ projet/scriptsql/5.1_Exercice_La_Poste.sql
@@ -0,0 +1,80 @@
+#========================================
+#             SCRIPT MYSQL
+#========================================
+
+DROP DATABASE IF EXISTS ddd;
+CREATE DATABASE ddd;
+USE ddd;
+
+#========================================
+# Table : Bureaux
+#========================================
+CREATE TABLE Bureaux(
+	idBureau INT AUTO_INCREMENT PRIMARY KEY ,
+	codePostal VARCHAR(6) 
+)ENGINE = InnoDB;
+
+#========================================
+# Table : Types
+#========================================
+CREATE TABLE Types(
+	idType INT AUTO_INCREMENT PRIMARY KEY ,
+	libelleType VARCHAR(50) 
+)ENGINE = InnoDB;
+
+#========================================
+# Table : Courriers
+#========================================
+CREATE TABLE Courriers(
+	idCourrier INT AUTO_INCREMENT PRIMARY KEY ,
+	rueDestinataire VARCHAR(150) NOT NULL ,
+	numDestintaire VARCHAR(5) NOT NULL ,
+	rueEmetteur VARCHAR(150)  ,
+	numEmetteur VARCHAR(50)  ,
+	idType INT NOT NULL
+)ENGINE = InnoDB;
+
+#========================================
+# Table : Transports
+#========================================
+CREATE TABLE Transports(
+	idTransport INT AUTO_INCREMENT PRIMARY KEY ,
+	libelleTransport VARCHAR(50)  ,
+	taxeCarbonne INT
+)ENGINE = InnoDB;
+
+#========================================
+# Table : Centres_de_tri
+#========================================
+CREATE TABLE Centres_de_tri(
+	idCentresDeTri INT AUTO_INCREMENT PRIMARY KEY ,
+	nomCentreDeTri VARCHAR(50) 
+)ENGINE = InnoDB;
+
+#========================================
+# Table : Achemine
+#========================================
+CREATE TABLE Achemine(
+	idAchemine INT AUTO_INCREMENT PRIMARY KEY ,
+	idBureau INT  ,
+	idTransport INT  ,
+	idCentresDeTri INT 
+)ENGINE = InnoDB;
+
+#========================================
+# Table : Gere
+#========================================
+CREATE TABLE Gere(
+	idGere INT AUTO_INCREMENT PRIMARY KEY ,
+	idBureau INT  ,
+	idCourrier INT  ,
+	dateEnvoi DATE ,
+	dateReception DATE
+)ENGINE = InnoDB;
+
+ALTER TABLE Courriers ADD CONSTRAINT FK_Courriers_Types FOREIGN KEY(idType) REFERENCES Types(idType);
+ALTER TABLE Achemine ADD CONSTRAINT FK_Achemine_Bureaux FOREIGN KEY(idBureau) REFERENCES Bureaux(idBureau);
+ALTER TABLE Achemine ADD CONSTRAINT FK_Achemine_Transports FOREIGN KEY(idTransport) REFERENCES Transports(idTransport);
+ALTER TABLE Achemine ADD CONSTRAINT FK_Achemine_Centres_de_tri FOREIGN KEY(idCentresDeTri) REFERENCES Centres_de_tri(idCentresDeTri);
+ALTER TABLE Gere ADD CONSTRAINT FK_Gere_Bureaux FOREIGN KEY(idBureau) REFERENCES Bureaux(idBureau);
+ALTER TABLE Gere ADD CONSTRAINT FK_Gere_Courriers FOREIGN KEY(idCourrier) REFERENCES Courriers(idCourrier);
--- projet/scriptsql/test.php
+++ projet/scriptsql/test.php
@@ -1,38 +1,105 @@
 <?php
-    $xml = simplexml_load_file("test.xml");
-    $json = json_encode($xml);;
-    $myArray = json_decode($json,true);
+$xml = simplexml_load_file("test.xml");
+$json = json_encode($xml);
+$myArray = json_decode($json, true);
 
-    var_dump ($myArray);
 
-    /*
-    MyArray['Table'][i]['Name'] => name = Nom de la table
-    MyArray['Table'][i]['Column'] => C'est un Tableau avec des tableaux qui seront les colonnes de la table
-    MyArray['Table'][i]['Column'][i]['Name'] => Nom de la colonne
-    MyArray['Table'][i]['Column'][i]['Type'] => Type de la colonne
-    MyArray ['Table][i]['PK'][0]['Key'] => les clés primaires /!\ si la clé primaire contient une ',' c'est une table associative !
-    MyArray ['Table][i]['FK'][i]['Key'] => le nom de la clé étrangère
-    MyArray ['Table][i]['FK'][i]['Reference'] => nom de la références (c'est le truc qui a apres references en SQL)
-    */
-    $SQLFile = fopen($myArray['Name'].'.sql', 'w');
+/*
+$myArray['Table'][$i]['Name'] => name = Nom de la table
+$myArray['Table'][$i]['Column'] => C'est un Tableau avec des tableaux qui seront les colonnes de la table
+$myArray['Table'][$i]['Column'][$i]['Name'] => Nom de la colonne
+$myArray['Table'][$i]['Column'][$i]['Type'] => Type de la colonne
+$myArray['Table'][$i]['PK']['Key'] => les clés primaires /!\ si la clé primaire contient une ',' c'est une table associative !
+$myArray['Table'][$i]['FK'][$i]['Key'] => le nom de la clé étrangère
+$myArray['Table'][$i]['FK'][$i]['Reference'] => nom de la références (c'est le truc qui a apres references en SQL)
+ */
 
-    $SQLSnippet = "DROP DATABASE IF EXISTS ".$myArray['Name'].';'
-    ."\nCREATE DATABASE ".$myArray['Name'].';'
-    ."\nUSE ".$myArray['Name'].';';
+$fileName = str_replace(" ", "_", $myArray['Name']);
+$SQLFile = fopen($fileName . '.sql', 'w');
 
-    //var_dump (count($myArray['Table']));
-    for ($i=0;$i<count($myArray['Table']);$i++)
-    {
-        $SQLSnippet .= "\n#========================================\n
-        # Table : ".$myArray['Table'][$i]['Name']."
-        \n#========================================\n";
+$SQLSnippet = "#========================================\n"
+    . "#             SCRIPT MYSQL"
+    . "\n#========================================\n"
+    . "\nDROP DATABASE IF EXISTS " . $fileName . ';'
+    . "\nCREATE DATABASE " . $fileName . ';'
+    . "\nUSE " . $fileName . ';';
 
-        //var_dump($myArray['Table'][$i]['Name']);
+$tableNameArr = [];
+$tabFK = [];
+$cpt = 0;
 
-        // for ($j=0;$j<count($myArray['Table'][$i]['Column']);$j++)
-        // {
-        //     // var_dump($myArray['Table'][$i]['Column'][$j]['Name']);
-        // }
+// On boucle sur les tables ..
+for ($i = 0; $i < count($myArray['Table']); $i++) {
+    // on regarde si la table contient des Foreign Keys ..
+    if (isset($myArray['Table'][$i]['FK'])) {
+
+        // On regarde si dans les Fk il y en a qu'une ou plusieurs, SI $myArray['Table'][$i]['FK'] contient des tableaux associatifs c'est qu'il y a qu'un Fk sinon Il y en a plusieurs.
+        if (!(is_array($myArray['Table'][$i]['FK']) && array_diff_key($myArray['Table'][$i]['FK'], array_keys(array_keys($myArray['Table'][$i]['FK']))))) {
+            // On récupères les FK dans un tableau
+            for ($j = 0; $j < count($myArray['Table'][$i]['FK']); $j++) {
+                $tableNameArr[] = $myArray['Table'][$i]['Name'];
+                $tabFK[] = [$myArray['Table'][$i]['FK'][$j]['Key'], $myArray['Table'][$i]['FK'][$j]['Reference']];
+            }
+        } else {
+            // On récupère les FK dans un tableau
+            $tableNameArr[] = $myArray['Table'][$i]['Name'];
+            $tabFK[] = [$myArray['Table'][$i]['FK']['Key'], $myArray['Table'][$i]['FK']['Reference']];
+        }
+        $cpt++;
     }
+
+    // on affiche la table et ses attributs
+    $SQLSnippet .= "\n\n#========================================\n"
+        . "# Table : " . $myArray['Table'][$i]['Name']
+        . "\n#========================================\n"
+        . "CREATE TABLE " . $myArray['Table'][$i]['Name'] . "(\n";
+
+
+    if (strpos($myArray['Table'][$i]['PK']['Key'],",") != false)
+    {
+        // Ici on met une nouvelle primary key quand c'est une tablea associative mais il faut quand même check apres si on nous envoie encore des INT AUTO_INCREMENT pourles tranformaer en INT
+        $SQLSnippet .= "\t" . "id".$myArray['Table'][$i]['Name']." INT AUTO_INCREMENT PRIMARY KEY ,";
+
+        if ($myArray['Table'][$i]['Column'][0]['Type'] == "INT AUTO_INCREMENT") {
+            $SQLSnippet .= "\n\t" . $myArray['Table'][$i]['Column'][0]['Name'] . " INT ";
+        } else {
+            $SQLSnippet .= "\t" . $myArray['Table'][$i]['Column'][0]['Name'] . " " . $myArray['Table'][$i]['Column'][0]['Type'];
+        }
+    }else {
+    // Le premier attribut c'est forcément un id MAIS on verifie si celle ci est un int AUTO_INCREMENT sinon c'est un id Naturel d
+    // A VOIR ICI SI C'EST UN ID NATUREL EST IL UNE PRIMARY KEY ?
+    if ($myArray['Table'][$i]['Column'][0]['Type'] == "INT AUTO_INCREMENT") {
+        $SQLSnippet .= "\t" . $myArray['Table'][$i]['Column'][0]['Name'] . " " . $myArray['Table'][$i]['Column'][0]['Type'] . " PRIMARY KEY";
+    } else {
+        $SQLSnippet .= "\t" . $myArray['Table'][$i]['Column'][0]['Name'] . " " . $myArray['Table'][$i]['Column'][0]['Type'];
+    }
+}
+    for ($g = 1; $g < count($myArray['Table'][$i]['Column']); $g++) {
+        
+        if ($g < count($myArray['Table'][$i]['Column'])) {
+            $SQLSnippet .= " ,";
+        }
+
+        // Quand un MLD fait mets un FK dans une table il le type en INT AUTO_INCREMENT sauf que c'est une FK on veut juste le typer en INT donc on verifie ici.
+        if ($myArray['Table'][$i]['Column'][$g]['Type'] == "INT AUTO_INCREMENT") {
+            $SQLSnippet .= "\n\t" . $myArray['Table'][$i]['Column'][$g]['Name'] . " INT ";
+        } else {
+            $SQLSnippet .= "\n\t" . $myArray['Table'][$i]['Column'][$g]['Name'] . " " . $myArray['Table'][$i]['Column'][$g]['Type'];
+        }
+        if (isset($myArray['Table'][$i]['Column'][$g]['Property'])) {
+            $SQLSnippet .= $myArray['Table'][$i]['Column'][$g]['Property'];
+        }
+    }
+    $SQLSnippet .= "\n)ENGINE = InnoDB;";
+
+}
+
+
+// On sépare pour les alter table
+$SQLSnippet .= "\n";
+
+for ($k = 0; $k < count($tableNameArr); $k++) {
+    $SQLSnippet .= "\nALTER TABLE " . $tableNameArr[$k] . " ADD CONSTRAINT FK_" . $tableNameArr[$k] . "_" . stristr($tabFK[$k][1], "(", true) . " FOREIGN KEY(" . $tabFK[$k][0] . ") REFERENCES " . $tabFK[$k][1] . ";";
+}
 
 fputs($SQLFile, $SQLSnippet);
