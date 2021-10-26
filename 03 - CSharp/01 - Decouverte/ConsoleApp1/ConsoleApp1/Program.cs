6471184ce48c249fb306d63213a81127bc19c99f
Quentin BALAIR <quentin.balair2706@gmail.com>
2021-10-26 10:50:57 +0200

    26/10/2021 10:50:55,16

--- 03 - CSharp/01 - Decouverte/ConsoleApp1/ConsoleApp1/Program.cs
+++ 03 - CSharp/01 - Decouverte/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,17 +1,15 @@
-﻿using System;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
-    class Program
+    using System;
+
+    internal class Program
     {
-        static void Main(string[] args)
+        internal static void Main(string[] args)
         {
-            /*string test;
-            Console.Write("entre un truc :");
-            test = Console.ReadLine();
-            Console.WriteLine("Result : "+test+" voila");
-            Console.WriteLine();*/
-            //Console.WriteLine(Program.Fonction());
+            //string test;
+            //Console.Write("entre un truc :");
+            //test = Console.ReadLine();
+            //Console.WriteLine("Result : " + test + " voila");
 
             // 1 - Variables 
             // 1.1.1
@@ -26,12 +24,14 @@ namespace ConsoleApp1
             /*string entier;
 
             Console.Write("Saisir un entier : ");
+
             entier = Console.ReadLine();
-             try
+
+            try
             {
                 Convert.ToInt32(entier);
                 Console.Write("Votre entier est : " + entier);
-            } 
+            }
             catch
             {
                 Console.Write("Votre entier n'est pas un entier");
@@ -39,7 +39,7 @@ namespace ConsoleApp1
 
             // 1.2.1
 
-/*            string value1;
+            /*string value1;
             string value2;
             int val1 = 0;
             int val2 = 0;
@@ -80,16 +80,100 @@ namespace ConsoleApp1
             } while (!flag);
 
             int result1 = val1 + val2;
-            int result2 = val1 / val2;
             Console.WriteLine("La somme des deux est : " + result1);
-            Console.WriteLine("Le quotient des deux valeurs est : " + result2);*/
 
-        }
+            if (val2 != 0 || val1 != 0)
+            {
+                int result2 = val1 / val2;
+                Console.WriteLine("Le quotient des deux valeurs est : " + result2);
+            }
+            else
+            {
+                Console.WriteLine("Une division par 0 est impossible.");
+            }*/
 
-        static int Fonction()
-        {
-            int test = 3;
-            return test;
+            //1.3.1
+
+            //string value;
+            //bool flag;
+            //double val;
+            //do
+            //{
+            //    Console.WriteLine("Valeur :");
+            //    value = Console.ReadLine();
+
+            //    try
+            //    {
+            //        val = float.Parse(value);
+            //        Console.WriteLine("\nVotre float est : "+val);
+            //        flag = true;
+            //    }
+            //    catch
+            //    {
+            //        Console.WriteLine("\nvaleur incorrecte");
+            //        flag = false;
+            //    }
+            //} while (!flag);
+
+            //1.3.2
+
+            //Console.WriteLine("Saisissez 3 valeurs :");
+            //string value1 = Console.ReadLine();
+            //string value2 = Console.ReadLine();
+            //string value3 = Console.ReadLine();
+
+            //double moyenne = (float.Parse(value1) + float.Parse(value2) + float.Parse(value3)) / 3;
+
+            //Console.WriteLine("La moyenne est de :" + Math.Round(moyenne,2));
+
+            //1.3.3
+
+            //Console.Write("Saisissez la longueur : ");
+            //string value1 = Console.ReadLine();
+            //Console.Write("Saisissez la largeur : ");
+            //string value2 = Console.ReadLine();
+            //double surface = float.Parse(value1) * float.Parse(value2);
+
+            //Console.WriteLine("La surface du rectangle est :" + Math.Round(surface, 2));
+
+
+            // 1.4.1
+
+            //char character = 'a';
+            //Console.WriteLine((int)character);
+
+            // 1.4.2
+
+            //Console.Write("Saisissez un charactere en minuscule : ");
+            //string value = Console.ReadLine();
+            //Console.Write(value.ToUpper());
+
+
+            // 2.1
+            Console.Write("Saisissez un charactere : ");
+            char value = char.Parse(Console.ReadLine());
+            Console.WriteLine((char) ((int) value+1));
+
+            // 2.2
+            char val 1 = 0;
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
