711745b3de1bbb445e93117354b8923beae849b5
Quentin2706 <quentin.balair2706@gmail.com>
2021-11-02 17:38:15 +0100

    02/11/2021

--- 03 - CSharp/01 - Algo/exo4/exo4/Program.cs
+++ 03 - CSharp/01 - Algo/exo4/exo4/Program.cs
@@ -55,14 +55,14 @@ namespace exo4
             //    val = int.TryParse(Console.ReadLine(), out nb);
             //} while (!val || nb < 0);
 
-            //for (int i = 1; i <= 10; i++)
+            //for (int i = 0; i <= 10; i++)
             //{
             //    Console.WriteLine(nb + " x " + i + " = " + (nb * i));
             //}
 
 
-            //int[,] tab = new int[2, 10] { 
-            //    { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 
+            //int[,] tab = new int[2, 10] {
+            //    { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
             //    { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
             //};
 
@@ -118,117 +118,285 @@ namespace exo4
             // Calculette
 
 
-            double nb;
-            double nb2;
-            int nb3;
-            bool val;
-            char op;
-            double result;
-            bool flag;
-            bool flagKey;
+            //double nb;
+            //double nb2;
+            //int nb3;
+            //bool val;
+            //char op;
+            //double result;
+            //bool flag;
+            //bool flagKey;
 
-            flag = true;
-            flagKey = false;
-            result = 0;
-            nb = 0;
-            nb2 = 0;
-            nb3 = 0;
-            val = false;
+            //flag = true;
+            //flagKey = false;
+            //result = 0;
+            //nb = 0;
+            //nb2 = 0;
+            //nb3 = 0;
+            //val = false;
 
-            do
+            //do
+            //{
+
+            //    if (flag)
+            //    {
+            //        Console.Write("Entrez une valeur : ");
+            //        val = double.TryParse(Console.ReadLine(), out nb);
+            //        flag = false;
+            //    }
+
+            //    if (val || !flag)
+            //    {
+            //        Console.Write("Entrez un opérateur : ");
+            //        val = char.TryParse(Console.ReadLine(), out op);
+            //        if (val)
+            //        {
+            //            if (op != 'V' && op != '?')
+            //            {
+            //                if (op != '$')
+            //                {
+            //                    Console.Write("Entrez une valeur : ");
+            //                    val = double.TryParse(Console.ReadLine(), out nb2);
+            //                }
+            //                else
+            //                {
+            //                    do
+            //                    {
+            //                        Console.Write("Entrez une valeur entière : ");
+            //                        val = int.TryParse(Console.ReadLine(), out nb3);
+            //                    } while (!val);
+
+            //                }
+            //            }
+            //            switch (op)
+            //            {
+            //                case '+':
+            //                    result = nb + nb2;
+            //                    break;
+            //                case '-':
+            //                    result = nb - nb2;
+            //                    break;
+            //                case '/':
+            //                    result = nb / nb2;
+            //                    break;
+            //                case '*':
+            //                    result = nb * nb2;
+            //                    break;
+
+            //                case '$':
+            //                    result = Math.Pow(nb, nb3);
+            //                    break;
+
+            //                case 'V':
+            //                    result = Math.Sqrt(nb);
+            //                    break;
+
+            //                case '!':
+            //                    result = nb;
+            //                    for (int i = 1; i < nb; i++)
+            //                    {
+            //                        result *= i;
+            //                    }
+            //                    break;
+
+            //                default:
+            //                    val = false;
+            //                    break;
+            //            }
+
+            //            if (val)
+            //            {
+            //                Console.WriteLine(" = " + result);
+
+            //                if (!flag)
+            //                {
+            //                    nb = result;
+            //                    val = true;
+            //                }
+            //            }
+            //        }
+            //    }
+
+            //    if (val)
+            //    {
+            //        Console.WriteLine("Arrêter le calcul ? Press Enter");
+            //        if (Console.ReadKey(true).Key == ConsoleKey.Enter)
+            //        {
+            //            flagKey = true;
+            //        }
+            //    }
+
+
+            //} while (!val || !flagKey);
+
+            //Console.Write("Le résultat final est : " + result);
+
+
+            //static double DemanderDouble()
+            //{
+            //    bool val;
+            //    double nb;
+            //    val = true;
+
+            //    do
+            //    {
+            //        Console.Write("Entrez une valeur : ");
+            //        val = double.TryParse(Console.ReadLine(), out nb);
+                    
+            //        if (!val)
+            //            Console.WriteLine("Saisie incorrecte.");
+
+            //    } while (!val);
+
+            //    return nb;
+            //}
+
+            //// DemanderDouble();
+
+            //static double DemanderDoubleNonNull()
+            //{
+            //    bool val;
+            //    double nb;
+            //    val = true;
+
+            //    do
+            //    {
+            //        Console.Write("Entrez une valeur : ");
+            //        val = double.TryParse(Console.ReadLine(), out nb);
+
+            //        if (!val)
+            //            Console.WriteLine("Saisie incorrecte.");
+
+            //    } while (!val || nb != 0);
+            //    return nb;
+            //}
+
+            //// DemanderDoubleNonNull();
+
+            //static int DemanderEntierPositif()
+            //{
+            //    bool val;
+            //    int nb;
+            //    val = true;
+
+            //    do
+            //    {
+            //        Console.Write("Entrez une valeur : ");
+            //        val = int.TryParse(Console.ReadLine(), out nb);
+
+            //        if (!val || nb <= 0)
+            //            Console.WriteLine("Saisie incorrecte.");
+
+            //    } while (!val || nb <= 0);
+            //    return nb;
+            //}
+
+            //// DemanderEntierPositif();
+
+
+            static char DemanderOperateur()
             {
+                bool ok = true;
+                char op;
+                bool condition;
 
-                if (flag)
+                do
                 {
-                    Console.Write("Entrez une valeur : ");
-                    val = double.TryParse(Console.ReadLine(), out nb);
-                    flag = false;
-                }
+                    Console.Write("Entrez un opérateur +,-,*,/,$,!,V : ");
+                    ok = char.TryParse(Console.ReadLine(), out op);
+                    condition = !ok || (op != '+' && op != '-' && op != '*' && op != '/' && op != '$' && op != '!' && op != 'V' && op != 'v');
+                    if (condition)
+                        Console.WriteLine("Saisie incorrecte.");
 
-                if (val || !flag)
+                } while (condition);
+                return op;
+            }
+
+            //  DemanderOperateur();
+
+            static double Calcul(double valeur, char operateur)
+            {
+                double result;
+                result = 0;
+
+                switch (operateur)
                 {
-                    Console.Write("Entrez un opérateur : ");
-                    val = char.TryParse(Console.ReadLine(), out op);
-                    if (val)
-                    {
-                        if (op != '!' && op != '?')
-                        { 
-                            if (op != '$')
-                            { 
-                                Console.Write("Entrez une valeur : ");
-                                val = double.TryParse(Console.ReadLine(), out nb2);
-                            } else
-                            {
-                                do
-                                {
-                                    Console.Write("Entrez une valeur entière : ");
-                                    val = int.TryParse(Console.ReadLine(), out nb3);
-                                } while (!val);
-                          
-                            }
-                        }
-                        switch (op)
+                    case 'V':
+                        result = Math.Sqrt(valeur);
+                        break;
+
+                    case '!':
+                        result = Math.Round(valeur);
+                        for (int i = 1; i < valeur; i++)
                         {
-                            case '+':
-                                result = nb + nb2;
-                                break;
-                            case '-':
-                                result = nb - nb2;
-                                break;
-                            case '/':
-                                result = nb / nb2;
-                                break;
-                            case '*':
-                                result = nb * nb2;
-                                break;
-
-                            case '$':
-                                result = Math.Pow(nb,nb3);
-                                break;
-
-                            case '!':
-                                result = Math.Sqrt(nb);
-                                break;
-
-                            case '?':
-                                result = nb;
-                                for (int i = 1; i < nb; i++)
-                                {
-                                    result *= i;
-                                }
-                                break;
-
-                            default:
-                                val = false;
-                                break;
+                            result *= i;
                         }
-
-                        if (val)
-                        {
-                            Console.WriteLine(" = " + result);
-
-                            if (!flag)
-                            {
-                                nb = result;
-                                val = true;
-                            }
-                        }
-                    }
+                        break;
                 }
+                return result;
+            }
 
-                if (val)
-                {
-                    Console.WriteLine("Arrêter le calcul ? Press Enter");
-                    if (Console.ReadKey(true).Key == ConsoleKey.Enter)
-                    {
-                        flagKey = true;
-                    }
-                }
-                
+            //// CalculDouble(5.5 , 'V');
 
-            } while (!val || !flagKey);
 
-            Console.Write("Le résultat final est : "+result);
+            //static double CalculSimple(double valeur, char operateur, double valeur2)
+            //{
+            //    double result;
+            //    result = 0;
+
+            //    switch (operateur)
+            //    {
+            //        case '+':
+            //            result = valeur + valeur2;
+            //            break;
+            //        case '-':
+            //            result = valeur - valeur2;
+            //            break;
+            //        case '/':
+            //            result = valeur / valeur2;
+            //            break;
+            //        case '*':
+
+            //            result = valeur * valeur2;
+            //            break;
+
+            //        case '$':
+            //            result = Math.Round(valeur);
+            //            result = Math.Pow(valeur, valeur2);
+            //            break;
+            //    }
+            //    return result;
+            //}
+            //// Console.WriteLine(" = " + result);
+            //// CalculSimple(5.4, '$' ,6.8);
+
+            //static void Calculatrice()
+            //{
+            //    double valDouble1;
+            //    double valDouble2;
+            //    bool flag = true;
+            //    char op;
+            //    valDouble1 = DemanderDouble();
+
+
+
+            //    op = DemanderOperateur();
+
+            //    if (op == '/')
+            //    {
+            //        valDouble2 = DemanderDoubleNonNull();
+            //    }
+            //    else if (op == '$')
+            //    {
+
+            //    }
+            //    else
+            //    {
+            //        valDouble2 = DemanderDouble();
+            //    }
+            //}
+
+
         }
     }
 }
---
+++ 03 - CSharp/01 - Algo/exo5/exo5/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace exo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// 1
+            //string fff;
+            //fff = "Les framboises sont perchées sur le tabouret de mon grand-père.";
+
+            //for (int i = 0; i <= fff.Length; i++)
+            //{
+            //    Console.WriteLine(fff[i]);
+            //}
+
+
+            //// 2
+            //bool val;
+            //string t;
+            //int ti;
+            //int tj;
+            //string result;
+            //result = "";
+
+            //Console.Write("saisissez une chaine de caractères : ");
+            //t = Console.ReadLine();
+            //do
+            //{
+            //    Console.Write("saisissez ti : ");
+            //    val = int.TryParse(Console.ReadLine(), out ti);
+
+            //    Console.Write("saisissez tj : ");
+            //    val = int.TryParse(Console.ReadLine(), out tj);
+
+            //   if (!val)
+            //   {
+            //        Console.WriteLine("Entrez une valeur positive");
+            //   }
+            //} while (!val || ti < 0 || tj < 0 || tj<ti || ti > t.Length || tj > t.Length);
+
+            //for (int i = ti; i < tj; i++)
+            //{
+            //    result += t[i];
+            //}
+            //Console.Write(t+" "+result);
+
+            //3
+            //bool val;
+            //string t;
+            //int ti;
+            //int tj;
+            //string temp;
+            //temp = "";
+
+
+            //Console.Write("saisissez une chaine de caractères : ");
+            //t = Console.ReadLine();
+
+            //do
+            //{
+            //    Console.Write("saisissez ti : ");
+            //    val = int.TryParse(Console.ReadLine(), out ti);
+
+            //    Console.Write("saisissez tj : ");
+            //    val = int.TryParse(Console.ReadLine(), out tj);
+
+            //    if (!val)
+            //        Console.WriteLine("Entrez une valeur positive");
+
+            //} while (!val || ti < 0 || tj < 0 || tj < ti || ti > t.Length || tj > t.Length);
+
+            //for (int i = ti; i < tj; i++)
+            //{
+            //    temp += t[i];
+            //}
+
+            //Console.Write(t.Insert(t.Length," "+temp));
+
+
+
+            // 4
+            //string t;
+            //string a;
+            //string b;
+
+
+            //Console.Write("saisissez une chaine de caractères : ");
+            //t = Console.ReadLine();
+
+            //Console.Write("La lettre que vous voulez changer : ");
+            //a = Console.ReadLine();
+
+            //Console.Write("la lettre que vous voulez remplacer par "+a+" : ");
+            //b = Console.ReadLine();
+
+
+            //Console.WriteLine(t.Replace(a, b));
+
+            // 5
+            //string t;
+            //string a;
+            //string b;
+
+
+            //Console.Write("saisissez une chaine de caractères : ");
+            //t = Console.ReadLine();
+
+            //Console.Write("La lettre que vous voulez changer : ");
+            //a = Console.ReadLine();
+
+            //Console.Write("la lettre que vous voulez remplacer par "+a+" : ");
+            //b = Console.ReadLine();
+
+
+            //Console.WriteLine(t.Replace(a, b));
+
+            //6
+            string t;
+            int index;
+
+            Console.WriteLine("Entrez le nom d'un fichier : ");
+            t = Console.ReadLine();
+
+            index = t.LastIndexOf(".");
+
+
+            Console.WriteLine("nom de fichier : " + t.Substring(0, index));
+            Console.WriteLine("Extension : " + t.Substring(index));
+
+
+        }
+    }
+}
