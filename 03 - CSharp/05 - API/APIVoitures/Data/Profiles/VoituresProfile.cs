ebe8835d0748b8650ad810df43b7f4bac202149b
Quentin BALAIR <quentin.balair2706@gmail.com>
2021-11-25 10:51:02 +0100

    25/11/2021 10:50:58,76

--- 03 - CSharp/05 - API/APIVoitures/Data/Profiles/VoituresProfile.cs
+++ 03 - CSharp/05 - API/APIVoitures/Data/Profiles/VoituresProfile.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using APIVoitures.Data.Dtos;
+using APIVoitures.Data.Models;
+using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace APIVoitures.Data.Profiles
 {
-    public class VoituresProfile
-    {
-        CreateMap<Personne, PersonnesDTO>();
-        CreateMap<PersonnesDTO, Personne>();
+    public class VoituresProfile : Profile
+    {   
+        public VoituresProfile()
+        {
+            CreateMap<Voiture, VoituresDTO>();
+            CreateMap<VoituresDTO, Voiture>();
+        }
     }
 }
