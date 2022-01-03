868d763eb839266b988f4d240543802a6ba61bd1
Quentin2706 <quentin.balair2706@gmail.com>
2022-01-03 14:11:33 +0100

    03-01

--- 04 - Web/01 - Front End/02_JS/2048/script.js
+++ 04 - Web/01 - Front End/02_JS/2048/script.js
@@ -6,10 +6,7 @@
 const taille = 5;
 const randStart = 2;
 var grille = document.getElementById("grid");
-var lignes = [];
 var cases = [];
-
-var casesVide = document.getElementsByClassName("caseVide");
 gridSettings();
 /****************************/
 
@@ -21,46 +18,110 @@ function gridSettings() {
     let stringGrid = "";
     for (let i = 0; i < taille; i++) {
         stringGrid += "2fr ";
-        let ligne = [];
+        stringLigne = "l" + i;
         for (let j = 0; j < taille; j++) {
             let caseTab = document.createElement("div");
             caseTab.classList.add("caseVide");
+            caseTab.classList.add("center");
+            caseTab.id = "l" + i + "c" + j;
             grille.appendChild(caseTab);
-            ligne.push(caseTab);
             cases.push(caseTab);
         }
-        lignes.push(ligne);
     }
     grille.style.gridTemplateColumns = stringGrid;
-    // on boucle x fois pour avoir des cases pleines de 2 au départ
-    // for(let i = 0; i < randStart; i++)
-    // {
-    //     setRandomCase();
-    // }
-    
+
+    //on boucle x fois pour avoir des cases pleines de 2 au départ
+    for (let i = 0; i < randStart; i++) {
+        setRandomCase();
+    }
 }
 
 function setRandomCase() {
-    let rand = 24//Math.floor(Math.random() * (casesVide.length - 2 - 0 + 1)) + 0;
-    console.log("1"+casesVide[rand]);
-    casesVide[rand].textContent = "2";
-    console.log("2"+casesVide[rand]);
-    casesVide[rand].classList.add("case");
-    console.log("3"+casesVide[rand]);
-    casesVide[rand].classList.remove("caseVide");
+    let randl = Math.floor(Math.random() * taille);
+    let randc = Math.floor(Math.random() * taille);
+    let laCase = document.getElementById(`l${randl}c${randc}`);
+    if (laCase.textContent == "") {
+        laCase.textContent = "2";
+        laCase.classList.add("case");
+        laCase.classList.remove("caseVide");
+    } else {
+        setRandomCase();
+    }
 }
 
 
-
 // On check si la partie est terminée ou un coup est jouable.
-
-function Result() {
+function result() {
     var cpt = 0;
     do {
         cpt++;
-    } while (cases[cpt] != "");
+    } while (cases[cpt] != "" && cpt < taille * taille);
 
-    if (FlagCombinaison && cpt < cases.lenght - 1) {
+    if (FlagCombinaison && cpt < cases.length) {
         alert("Partie Terminée !");
+    }
+}
+
+document.addEventListener('keydown', (e) => {
+    if (!e.repeat) {
+        switch (e.key) {
+            case "ArrowRight":
+                for (let i = 0; i < cases.length; i++)
+            {
+                deplacerCase("d");
+            }
+                break;
+            case "ArrowLeft":
+                deplacerCase("g");
+                break;
+            case "ArrowUp":
+                deplacerCase("h");
+                break;
+            case "ArrowDown":
+                deplacerCase("b");
+                break;
+            default:
+                console.log("Pas la bonne touche");
+                break;
+        }
+    }
+});
+
+function deplacerCase(key) {
+    switch (key) {
+        case "d":
+            for (let i = 0; i < cases.length; i++)
+            {
+                let idCaseEnCours = cases[i].getAttribute("id").substring(cases[i].getAttribute("id").indexOf("c")+1)*1+1;
+                let caseSuiv = idCaseEnCours*1+1;
+                if(cases[i].textContent != "")
+                {
+                    for (let j = 0; j < taille-1; j++)
+                    {
+                        if (!cases[caseSuiv].getElementById().classList.contains("caseVide")) {
+                            if (cases[i+ 1].textContent == cases[i].textContent) {
+                                console.log("fezfze")
+                            }
+                            console.log("zzzzz")
+                        }    
+                    }
+                }
+            }
+            break;
+        case "g":
+
+            break;
+
+        case "h":
+
+            break;
+
+        case "b":
+
+            break;
+
+        default:
+            console.log("WTF !")
+            break;
     }
 }
